abb5d2d9dc48180aca0db742fa9c9eed48412445
Preston Hill <preston.leon.hill@gmail.com>
2022-12-05 10:13:15 -0600

    added tech blog

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -12,7 +12,7 @@ function Work() {
                     <a href="https://chewy441014.github.io/trail-picker/" target="blank" id="card1"><figure className="card1 card" ><p>Park Picker</p> </figure></a>
                     <a href="https://notes-tracker-preston.herokuapp.com/" target="blank"><figure className="card2 card"><p>Notes App</p> </figure></a>
                     <a href="https://chewy441014.github.io/Code-Quiz/" target="blank"><figure className="card3 card"><p>JavaScript Quiz</p> </figure></a>
-                    <a href="https://chewy441014.github.io/portfolio/" target="blank"><figure className="card4 card"><p>Portfolio</p> </figure></a>
+                    <a href="https://tech-blog-preston.herokuapp.com/" target="blank"><figure className="card4 card"><p>Tech Blog</p> </figure></a>
                     <a href="https://chewy441014.github.io/Travel-Weather-Check/" target="blank"><figure className="card5 card"> <p>Weather Dashboard</p> </figure></a>
                 </div>
             </section>
