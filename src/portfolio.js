72435d704200afa6e6b10328d2eab2f228c1c09f
Preston Hill <preston.leon.hill@gmail.com>
2023-03-31 16:54:56 -0500

    adjusted links for resume

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://preston-hill-portfolio.herokuapp.com/',
+  homepage: 'https://chewy441014.github.io/portfolio-react/',
   title: 'Preston Hill Portfolio',
 }
 
@@ -10,7 +10,7 @@ const about = {
   role: 'Full Stack Web Developer',
   description:
     'Full Stack Web Dev with experience in the MERN stack and JavaScript through the UT Austin Coding Bootcamp. With a background in mechanical engineering, I have ample experience with analytic problem solving, team projects and mechanical design. My recent work experiences have polished my communication, customer service, time management and learning ability both at Anton Paar and Starship Technology. During my first month at Starship, I pushed the Houston location to 100% robot health. At Anton Paar, I regularly provided consultations to peers when they were in the field and in need of root cause analysis and deeper technical knowledge. Finally, during the UT Austin Coding Bootcamp, one of the projects I contributed to was a website which can coordinate national parks data, location data, directions and weather data in a convenient format for planning nature outings; feel free to reach out for details. With these experiences and skills I have helped bring dozens of team projects to fruition, in undergrad, grad school, during employment and during the UT Austin Coding Bootcamp.',
-  resume: 'https://example.com',
+  resume: 'https://chewy441014.github.io/portfolio-react/Hill_Preston_Resume(1).pdf',
   social: {
     linkedin: 'https://www.linkedin.com/in/hillpreston/',
     github: 'https://github.com/chewy441014',
